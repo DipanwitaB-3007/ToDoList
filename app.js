f66f6fdc70e42943ba950277cd692a6dae12dd0c
Dipanwita Barik <dipanwitabarik3007@gmail.com>
2020-05-22 01:53:28 +0530

    Code update

--- app.js
+++ app.js
@@ -2,10 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use()
+
+app.set('view engine', 'ejs');
+//app.use()
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    today = new Date();
+    currentDay = today.getDay();
+    day = "";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+    }
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 
