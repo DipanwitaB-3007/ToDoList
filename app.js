455eb9fed9b912eea02913e1e756e03a25d371c5
Dipanwita Barik <dipanwitabarik3007@gmail.com>
2020-05-27 01:13:41 +0530

    Make code changes

--- app.js
+++ app.js
@@ -3,41 +3,64 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+newItems = ["Take medicines", "Do laundry"];
+workItems = [];
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-//app.use()
 
 app.get("/", function(req, res) {
+
     today = new Date();
-    currentDay = today.getDay();
-    day = "";
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-    }
+    options = {
+        dateStyle: "full"
+    };
+    day = today.toLocaleDateString("en-IN", options);
+
     res.render("list", {
-        kindOfDay: day
+        listTitle: day,
+        newListItems: newItems
     });
 });
 
+app.get("/work", function(req, res) {
+    title = "Work List";
+    res.render("list", {
+        listTitle: title,
+        newListItems: workItems
+    });
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.post("/", function(req, res) {
+    item = req.body.newItem;
+    if (req.body.listType == "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        newItems.push(item);
+        res.redirect("/");
+    }
+});
+
+app.post("/remove", function(req, res) {
+    item = req.body.removeItem;
+    if (req.body.listType == "Work") {
+        index = workItems.indexOf(item);
+        workItems.splice(index, 1);
+        res.redirect("/work");
+    } else {
+        index = newItems.indexOf(item);
+        newItems.splice(index, 1);
+        res.redirect("/");
+    }
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
